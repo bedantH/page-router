f23b82a6f81973930034bb8bc61b98478800f083
Bedant Hota <mohanesh.h2003@gmail.com>
2023-12-03 23:42:17 +0530

    directory optimized

--- index.ts
+++ index.ts
@@ -7,15 +7,44 @@ let pageDir: {
 } = {};
 
 fs.readdir(dir, (err, files) => {
-  if (err) console.error("Error: ", err);
+  if (err) console.error("Error (root): ", err);
 
   files.forEach((file) => {
-    pageDir = {
-      ...pageDir,
-      [file.toString().split(".")[0] === "index"
-        ? "/"
-        : `/${file.toString().split(".")[0]}`]: file,
-    };
+    if (file) {
+      fs.stat(
+        `${dir}/${file}`,
+        (_err: NodeJS.ErrnoException | null, stats: fs.Stats) => {
+          if (stats.isDirectory()) {
+            fs.readdir(`${dir}/${file}`, (err, files) => {
+              if (err) console.error("Error (isDir): ", err);
+
+              files.forEach((fs) => {
+                if (fs.toString().split(".")[0] === "index") {
+                  pageDir = {
+                    ...pageDir,
+                    [`/${file}`]: `/${file}/index.html`,
+                  };
+                } else {
+                  pageDir = {
+                    ...pageDir,
+                    [`/${file}/${
+                      fs.toString().split(".")[0]
+                    }`]: `/${file}/${fs}`,
+                  };
+                }
+              });
+            });
+          } else {
+            pageDir = {
+              ...pageDir,
+              [file.toString().split(".")[0] === "index"
+                ? "/"
+                : `/${file.toString().split(".")[0]}`]: file,
+            };
+          }
+        }
+      );
+    }
   });
 
   pageDir = pageDir;
@@ -26,9 +55,9 @@ createServer((req, res) => {
 
   if (req.url !== "/favicon.ico") {
     fs.readFile(
-      `./pages/${tempPages[req.url || "./pages/404.html"]}`,
+      `./pages/${tempPages[req.url || ""] || "/404.html"}`,
       (err, data) => {
-        if (err) console.error("Error: ", err);
+        if (err) console.error("Error (Server): ", err);
 
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
@@ -37,3 +66,4 @@ createServer((req, res) => {
     );
   }
 }).listen(3000);
+console.log("💻 Server: Successfully Running at 3000, serving: /");
---
+++ js/path.js
@@ -0,0 +1,63 @@
+const http = require("http");
+const fs = require("fs");
+
+const dir = "./pages";
+let pageDir = {};
+
+fs.readdir(dir, (err, files) => {
+  if (err) console.error("Error (root): ", err);
+
+  files.forEach((file) => {
+    if (file) {
+      fs.stat(`${dir}/${file}`, (_err, stats) => {
+        if (stats.isDirectory()) {
+          fs.readdir(`${dir}/${file}`, (err, files) => {
+            if (err) console.error("Error (isDir): ", err);
+
+            files.forEach((fs) => {
+              if (fs.toString().split(".")[0] === "index") {
+                pageDir = {
+                  ...pageDir,
+                  [`/${file}`]: `/${file}/index.html`,
+                };
+              } else {
+                pageDir = {
+                  ...pageDir,
+                  [`/${file}/${fs.toString().split(".")[0]}`]: `/${file}/${fs}`,
+                };
+              }
+            });
+          });
+        } else {
+          pageDir = {
+            ...pageDir,
+            [file.toString().split(".")[0] === "index"
+              ? "/"
+              : `/${file.toString().split(".")[0]}`]: file,
+          };
+        }
+      });
+    }
+  });
+});
+
+http
+  .createServer((req, res) => {
+    const tempPages = pageDir;
+
+    if (req.url !== "/favicon.ico") {
+      fs.readFile(
+        `./pages/${tempPages[req.url || ""] || "/404.html"}`,
+        (err, data) => {
+          if (err) console.error("Error (Server): ", err);
+
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          return res.end();
+        }
+      );
+    }
+  })
+  .listen(3000);
+
+console.log("💻 Server: Successfully Running at 3000, serving: /");
